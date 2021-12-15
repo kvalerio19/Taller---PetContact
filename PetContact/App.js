ee45c508bd552d8f92260990d88a986bf834b337
kvalerio19 <kvalerio19@hotmail.es>
2021-12-14 21:47:55 -0600

    Initial commit

---
+++ PetContact/App.js
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import MapView from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <MapView style={styles.map} 
+        initialRegion={{
+        latitude: 9.928069,
+        longitude: -84.090725,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
