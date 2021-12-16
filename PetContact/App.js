ec27bbdf10a6490ffa0af905e884204204f0750e
kvalerio19 <kvalerio19@hotmail.es>
2021-12-15 21:09:14 -0600

    changes

--- PetContact/App.js
+++ PetContact/App.js
@@ -4,11 +4,20 @@ import { StyleSheet, Text, View, Dimensions } from 'react-native';
 //import { Header } from 'react-native/Libraries/NewAppScreen';
 import Screen from './components/screen';
 
+
 export default function App() {
+  
   return (
-    <View >
+    <View  style={styles.container}>
       <Text style={styles.titleText}>PET CONTACT</Text>
-        <Screen></Screen>
+      <MapView style={styles.map} 
+          initialRegion={{
+          latitude: 9.928069,
+          longitude: -84.090725,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        />
         
       </View>
   );
@@ -19,11 +28,22 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: "bold",
     marginLeft: 1,
-    marginTop: 20,
+    marginTop: 95,
     color: '#fff',
     fontSize: 30,
     backgroundColor: '#2ec76e',
     textAlign: 'center',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#2ec76e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    //marginTop: 740,
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
 
--- PetContact/components/screen.js
+++ PetContact/components/screen.js
@@ -1,44 +1,37 @@
-import * as React from 'react';
-import MapView from 'react-native-maps';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
-//import { Header } from 'react-native/Libraries/NewAppScreen';
-//import Screen from './components/screen';
+// import * as React from 'react';
+// import MapView from 'react-native-maps';
+// import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
-
-
-export default function Screen() {
-  return (
-    <View style={styles.container}>
-        <View>
-            
-            <Text style={styles.titleText}>PET CONTACT</Text>
-          
-        </View>
-        <MapView style={styles.map} 
-          initialRegion={{
-          latitude: 9.928069,
-          longitude: -84.090725,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-        />
+// export default function Screen() {
+//   return (
+      
+//     <View style={styles.container}>
         
+//         <MapView style={styles.map} 
+//           initialRegion={{
+//           latitude: 9.928069,
+//           longitude: -84.090725,
+//           latitudeDelta: 0.0922,
+//           longitudeDelta: 0.0421,
+//         }}
+//         />
         
-      </View>
-  );
-}
+//       </View>
+      
+//   );
+// }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#2ec76e',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  map: {
-    marginTop: 740,
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  },
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#2ec76e',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   map: {
+//     marginTop: 740,
+//     width: Dimensions.get('window').width,
+//     height: Dimensions.get('window').height,
+//   },
   
-});
+// });
